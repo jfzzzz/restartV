97cfbf5e044d8103249b2ada827178645ba7651d
郑天才 <28149536@qq.com>
2020-03-02 17:09:51 +0800

    端口堵塞

--- vue.config.js
+++ vue.config.js
@@ -45,7 +45,7 @@ module.exports = {
   devServer: {
     open: process.platform === 'darwin',
     // host: '0.0.0.0',
-    port: 8082,
+    port: 8080,
     https: false,
     hotOnly: false,
     proxy: {
